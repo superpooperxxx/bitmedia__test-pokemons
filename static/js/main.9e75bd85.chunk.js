a9f574c08833300a5a438c1c88df2aef7101e583
superpooperxxx <superpooperxxx@users.noreply.github.com>
2023-04-08 19:51:34 +0000

    deploy: 4a8936cd97a93c4719ea50c869376d2cd595b470

---
+++ static/js/main.9e75bd85.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-typescript-starter-pack"]=this["webpackJsonpreact-typescript-starter-pack"]||[]).push([[0],{10:function(t,e,r){"use strict";r.r(e);var c=r(0),s=r.n(c),n=r(3),a=r.n(n),i=(r(8),r(1)),o=function(){return Object(i.jsx)("div",{className:"starter",children:"Pokemons"})};a.a.render(Object(i.jsx)(s.a.StrictMode,{children:Object(i.jsx)(o,{})}),document.getElementById("root"))},8:function(t,e,r){}},[[10,1,2]]]);
+//# sourceMappingURL=main.9e75bd85.chunk.js.map
