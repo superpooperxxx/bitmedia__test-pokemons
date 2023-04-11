7eb9895401b332adc16d080d9156999bae21c68d
Kyrylo Haiduk <49764881+superpooperxxx@users.noreply.github.com>
2023-04-12 00:42:22 +0300

    added not found page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import './NotFoundPage.scss';
+import NotFound from '../../static/not-found.gif';
 
 export const NotFoundPage: React.FC = () => {
-  return <div>Not Found</div>;
+  return (
+    <main className="not-found-page">
+      <div className="container">
+        <h2 className="not-found-page__heading">Not Found</h2>
+        <img
+          src={NotFound}
+          alt="Not found"
+          className="not-found-page__img"
+        />
+      </div>
+    </main>
+  );
 };
